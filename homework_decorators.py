d2153631b03507bbb0968365e5a7a6fffa043717
Maria Maksymenko <136785612+Maksikd62@users.noreply.github.com>
2024-12-10 11:56:48 +0200

    homework_decorators

--- homework_decorators.py
+++ homework_decorators.py
@@ -1,22 +1,40 @@
 import time
-#decorators (homework1)
-def allTime(func):
+#decorators (exercise1)
+def all_time(func):
     def difference():
-        startTime=time.time()
+        start_time=time.time()
         result=func()
-        endTime=time.time()
-        differenceTime=endTime-startTime
-        return differenceTime, result
+        end_time=time.time()
+        difference_time=end_time-start_time
+        return difference_time, result
     return difference
 
-@allTime
+@all_time
 def numbers():
-    evenNumbers=[]
+    even_numbers=[]
     for num in range(1, 100000):
         if num%2==0:
-            evenNumbers.append(num)
-    return evenNumbers
+            even_numbers.append(num)
+    return even_numbers
 
-timeForFunc, evenNumbers = numbers()
-print(evenNumbers)
-print("Time for function: "+ str(timeForFunc))
+time_for_func, even_numbers = numbers()
+print(even_numbers)
+print("Time for function: "+ str(time_for_func))
+
+#decorators (exercise2)
+def fix_params(func):
+    def wrapper(args):
+        new_args = []
+        for arg in args:
+            if isinstance(arg, (int, float)) and arg < 0:
+                new_args.append(abs(arg))
+            else:
+                new_args.append(arg)
+        return func(new_args)
+    return wrapper
+
+@fix_params
+def example_func(args):
+    return args
+
+print(example_func([10, -3, "red", -1, 200, -5, 2]))
